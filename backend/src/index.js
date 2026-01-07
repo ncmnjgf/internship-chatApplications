0074654f01d258d2a8b45061e66a9c4e9e054cae
Dadaso <bandgardadaso47@gmail.com>
2026-01-07 15:21:14 +0530

    routes are edited

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 import authRoutes from "./routes/auth.route.js"
-import dotenv from "dotenv";
+import dotenv, { parse } from "dotenv";
 import express from "express";
-import {connectDB} from "./lib/db.js"
+import {connectDB} from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT
 
-
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
     console.log(`server is running on the port ${PORT}`);
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,14 @@
 //for autications
-import{login , logout,signup} from "../controllers/auth.controller.js"
+import{checkAuth ,updateProfile ,login , logout,signup} from "../controllers/auth.controller.js"
 import express from "express"
-
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router()
 
-router.post("/signup", signup)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.put("/update-profile",protectRoute,updateProfile)
+router.get("/check",protectRoute,checkAuth)
+//protect route to check user is authenticated
 
-router.post("/login", login)
-router.post("/logout", logout)
 export default router;
