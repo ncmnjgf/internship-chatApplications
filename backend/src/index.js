67545b12972181289b1e75fc5ee20d0b7f438a4d
Dadaso <bandgardadaso47@gmail.com>
2026-02-17 20:45:16 +0530

    added new features

--- backend/src/index.js
+++ backend/src/index.js
@@ -52,6 +52,5 @@ if (process.env.NODE_ENV === "production") {
 /* ================= START SERVER ================= */
 server.listen(PORT, async () => {
   console.log(`🚀 Server running on port ${PORT}`);
-  console.log("server is connected susscefully");
 
 });
