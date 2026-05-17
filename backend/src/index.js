7d34074b468629ba116e09de8153c19de14e035d
Dadaso <bandgardadaso47@gmail.com>
2026-05-17 21:06:05 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === "production") {
 
 /* ================= START SERVER ================= */
 server.listen(PORT, async () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server ${PORT}`);
 
 
 });
