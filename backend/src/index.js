2e76c39b1bad6574edf9306396d7a1edc942a206
Dadaso <bandgardadaso47@gmail.com>
2026-07-11 17:40:32 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === "production") {
 
 /* ================= START SERVER ================= */
 server.listen(PORT, async () => {
-  console.log(`🚀 Server is connected successfully  ${PORT}`);
+  console.log(`🚀 Server  ${PORT}`);
 
 
 });
